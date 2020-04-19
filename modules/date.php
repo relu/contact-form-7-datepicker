286315cdbac5eec540006f975301418084d5c502
Frugan <dev@frugan.it>
2020-04-19 19:10:14 +0200

    Fixed deprecated WPCF7_Shortcode() Fixed deprecated create_function()

--- modules/date.php
+++ modules/date.php
@@ -29,7 +29,7 @@ class ContactForm7Datepicker_Date {
 	}
 
 	public static function shortcode_handler($tag) {
-		$tag = new WPCF7_Shortcode($tag);
+		$tag = new WPCF7_FormTag($tag);
 
 		if (empty($tag->name))
 			return '';
